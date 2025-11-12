41c0e64b852c0b80c35f126718d1c1dd461e1627
carlos-oliveira-codes <severino.csilva19@gmail.com>
2025-11-12 14:27:40 -0300

    Feat: Criando conexao ao bd com doctrine.php

---
+++ src/app/Config/doctrine.php
@@ -0,0 +1,29 @@
+<?php 
+
+    require_once __DIR__ . '/../../vendor/autoload.php';
+
+    use Doctrine\ORM\EntityManager;
+    use Doctrine\ORM\ORMSetup;
+    use Doctrine\DBAL\DriverManager;
+
+    $caminhoEntidade = [__DIR__ . '/../Entity'];
+    $caminhoProxies = __DIR__ . '/../Proxies';
+
+    $config = ORMSetup::createAttributeMetadataConfiguration($caminhoEntidade, true, $caminhoProxies);
+
+    $parametrosConexao = [
+
+        'dbname' => 'supermercado_tres_glorias',
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'driver' => 'pdo_mysql',
+        'charset' => 'utf8'
+    ];
+
+    $conexao = DriverManager::getConnection($parametrosConexao, $config);
+    $entityManager = new EntityManager($conexao, $config);
+
+
+
+?>
